329461e00bc520bc6b247cf1e6079917ba5a9aad
Elif Sıla Taş <122424595+elifsilatas@users.noreply.github.com>
2023-11-18 21:21:49 +0300

    temelAlgoritmaEgitimi
    btk

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace denemeler1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = Convert.ToInt32(
+                Console.ReadLine());
+
+            if (sayi % 2 == 0)
+                Console.WriteLine($"{sayi} çift sayıdır.");
+            else
+                Console.WriteLine($"{sayi} Tek sayıdır.");
+
+            Console.ReadKey();
+        }
+    }
+}
